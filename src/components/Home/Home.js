dec152114fa7a8b438af8a3194be752c1503793c
unknown <mdadib300@gmail.com>
2025-11-22 13:53:33 +0600

    Alert added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Banner from '../Banner/Banner';
 import Portfolio from '../Portfolio/Portfolio';
 import Skills from '../Skills/Skills';
@@ -7,6 +6,9 @@ import Testimonials from '../Testimonials/Testimonials';
 const Home = () => {
     return (
         <div>
+            <div>
+                <p className='text-4xl text-red-600 underline font-bold text-center'>Portfolio is under construction. Come later when completed.</p>
+            </div>
             <Banner />
             <Skills />
             <Portfolio />
